f0c03507c86646669e02bf0dc2f540c616a8bf63
Mick <michael_song@berkeley.edu>
2021-09-08 13:37:59 -0700

    Create scripts1.js

---
+++ AntiWar/js/scripts1.js
@@ -0,0 +1,18 @@
+var date1 = new Date("08/30/2021")
+var date2 = new Date()
+var Difference_In_Time = date2.getTime() - date1.getTime();
+var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
+function update_users_count() {
+    $('#users1 z').animate({
+        counter: Difference_In_Days * 300000000 /328200000
+    }, {
+        duration: 6000,
+        easing: 'swing',
+        step: function(now) {
+            $(this).text(Math.ceil(now));
+        },
+        complete: update_users_count
+    });
+};
+
+update_users_count();
